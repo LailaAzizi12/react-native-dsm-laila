cbb028a1998ff72ce7770363bb4422776987c8be
Laila <azizi.122844@e.unavarra.es>
2026-03-31 16:29:39 +0200

    Stack Navigation

--- componentes/DetalleExcursionComponent.js
+++ componentes/DetalleExcursionComponent.js
@@ -1,11 +1,19 @@
+import { Component } from "react";
 import { View, StyleSheet } from "react-native";
 import { Card, Text } from "react-native-paper";
+import { EXCURSIONES } from "../comun/excursiones";
+
 function RenderExcursion(props) {
   const excursion = props.excursion;
+
   if (excursion != null) {
     return (
       <Card style={styles.card}>
-        <Card.Title title={excursion.nombre} />
+        <Card.Title
+          title={excursion.nombre}
+          titleStyle={styles.titulo}
+          style={styles.cardTitle}
+        />
         <Card.Cover
           source={require("./imagenes/40Años.png")}
           style={styles.image}
@@ -20,8 +28,19 @@ function RenderExcursion(props) {
   }
 }
 
-function DetalleExcursion(props) {
-  return <RenderExcursion excursion={props.excursion} />;
+class DetalleExcursion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      excursiones: EXCURSIONES,
+    };
+  }
+
+  render() {
+    const { excursionId } = this.props.route.params;
+
+    return <RenderExcursion excursion={this.state.excursiones[+excursionId]} />;
+  }
 }
 
 const styles = StyleSheet.create({
@@ -34,6 +53,12 @@ const styles = StyleSheet.create({
   descripcion: {
     marginTop: 20,
     marginBottom: 20,
+  },
+  titulo: {
+    textAlign: "center",
+  },
+  cardTitle: {
+    alignItems: "center",
   },
 });
 
